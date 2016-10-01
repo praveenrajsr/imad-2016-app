52ddc471559d2580b6b6f88389c8900ee34aaf31
praveenrajsr <aashooshome@gmail.com>
2016-10-01 11:58:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,3 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'This is Great';
 
-//move the dino
-var img = document.getElementById('madi');
- img.onClick = function() {
-  img.style.marginLeft = '200px';  
-};
