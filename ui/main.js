70d3473e132cb234a63613d5af76597b0da8847e
praveenrajsr <aashooshome@gmail.com>
2016-10-02 22:07:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //change the content of the file
 
@@ -15,4 +15,25 @@ function moveright(){
 }
  img.onclick = function() {
   var interval = setInterval(moveright, 50);
+};*/
+
+//counter code
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    
+    //make a request to the counter end point
+    
+    //capture the response and store it in a variable
+    
+    //render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
+
+
+
+
+
