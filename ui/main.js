808dd452f57e6aead394c16415e9ca9dc5054b69
praveenrajsr <aashooshome@gmail.com>
2016-10-02 22:30:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,17 +20,26 @@ function moveright(){
 //counter code
 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
     
-    //make a request to the counter end point
-    
+    //create a request object
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+      if(request.readyState == XMLHttpRequest.DONE){
+          //take some action
+          if(requeststatus == 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+              
+          }
+      }  
+    };
+    //make the request
+    request.open('GET' , 'http://praveenrajsr.imad.hasura-app.io/counter' , true);
+    request.send(null);
     
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
 };
 
 
